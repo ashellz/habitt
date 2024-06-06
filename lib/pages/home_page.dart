16873939043c97f2fadd9f001e65fba1c8320a85
ashellz <101022734+ashellz@users.noreply.github.com>
2024-06-06 22:26:54 +0200

    Update
    - At the process of implementing the design for the categories

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:habit_tracker/data/habit_tile.dart';
 import 'package:habit_tracker/pages/icons.dart';
 import 'package:habit_tracker/util/HabitTile.dart';
@@ -26,6 +27,135 @@ class HomePage extends StatefulWidget {
 class HomePageState extends State<HomePage> {
   var habitListLenght = Hive.box<HabitData>('habits').length;
 
+  void createNewTask() {
+    setState(() {
+      myHabit = HabitData(
+          name: createcontroller.text,
+          completed: false,
+          icon: getIconString(updatedIcon.icon),
+          category: dropDownValue);
+      habitBox.add(myHabit);
+      habitListLenght = habitBox.length;
+    });
+    dropDownValue = 'Any time';
+    createcontroller.clear();
+    updatedIcon = startIcon;
+    Navigator.pop(context);
+  }
+
+  Future<void> deleteTask(int index) async {
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: Color.fromARGB(255, 218, 211, 190),
+          content: SizedBox(
+            height: 122,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Center(
+                  child: Text(
+                    textAlign: TextAlign.center,
+                    "Are you sure you want to delete this task?",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 152, 26, 51),
+                      ),
+                      child: const Text(
+                        "Delete",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          habitBox.deleteAt(index);
+                          habitListLenght = habitBox.length;
+                        });
+
+                        deleted = true;
+                        editcontroller.text = "";
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 37, 67, 54),
+                      ),
+                      child: const Text("Cancel",
+                          style: TextStyle(color: Colors.white)),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void editTask(int index) {
+    setState(() {
+      habitBox.putAt(
+          index,
+          HabitData(
+            name: editcontroller.text,
+            completed: habitBox.getAt(index)?.completed ?? false,
+            icon: getIconString(updatedIcon.icon),
+            category: dropDownValue,
+          ));
+    });
+    editcontroller.text = "";
+    updatedIcon = startIcon;
+    Navigator.pop(context);
+  }
+
+  void checkTask(int index) {
+    setState(() {
+      final habitBox = Hive.box<HabitData>('habits');
+      final existingHabit = habitBox.getAt(index);
+
+      if (existingHabit != null) {
+        final updatedHabit = HabitData(
+          name: existingHabit.name,
+          completed: !existingHabit.completed,
+          icon: existingHabit.icon,
+          category: existingHabit.category,
+        );
+
+        habitBox.putAt(index, updatedHabit);
+      }
+    });
+  }
+
+  String? _validateText(String? value) {
+    if (value?.isEmpty ?? true) {
+      return 'Please enter some text';
+    }
+    return null;
+  }
+
+  // THE SCAFFOLD STARTS HERE, ALL ABOVE ARE FUNCTIONS AND DECLARATIONS
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,13 +166,112 @@ class HomePageState extends State<HomePage> {
         toolbarHeight: 80.0,
         backgroundColor: const Color.fromARGB(255, 37, 67, 54),
       ),
-      body: ListView.builder(
-          itemCount: habitListLenght,
-          itemBuilder: (context, index) => HabitTile(
-              edittask: editTask,
-              deletetask: deleteTask,
-              checkTask: checkTask,
-              index: index)),
+      body: ListView(children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 10, top: 10, right: 10),
+              child: Container(
+                  width: double.infinity,
+                  height: 80,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(15)),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: const GridTile(
+                      footer: Center(
+                          child: Text(
+                        "Morning",
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      )),
+                      child: Image(
+                          fit: BoxFit.cover,
+                          image:
+                              AssetImage('assets/images/morning_picture.png')),
+                    ),
+                  )),
+
+              /*child: Text(
+                "Morning",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),*/
+            ),
+            for (int i = 0; i < habitListLenght; i++)
+              if (habitBox.getAt(i)?.category == 'Morning')
+                HabitTile(
+                  edittask: editTask,
+                  deletetask: deleteTask,
+                  checkTask: checkTask,
+                  index: i,
+                ),
+            const Padding(
+              padding: EdgeInsets.only(left: 20.0, top: 20),
+              child: Text(
+                "Afternoon",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            for (int i = 0; i < habitListLenght; i++)
+              if (habitBox.getAt(i)?.category == 'Afternoon')
+                HabitTile(
+                  edittask: editTask,
+                  deletetask: deleteTask,
+                  checkTask: checkTask,
+                  index: i,
+                ),
+            const Padding(
+              padding: EdgeInsets.only(left: 20.0, top: 20),
+              child: Text(
+                "Evening",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            for (int i = 0; i < habitListLenght; i++)
+              if (habitBox.getAt(i)?.category == 'Evening')
+                HabitTile(
+                  edittask: editTask,
+                  deletetask: deleteTask,
+                  checkTask: checkTask,
+                  index: i,
+                ),
+            const Padding(
+              padding: EdgeInsets.only(left: 20.0, top: 20),
+              child: Text(
+                "Any time",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            for (int i = 0; i < habitListLenght; i++)
+              if (habitBox.getAt(i)?.category == 'Any time')
+                HabitTile(
+                  edittask: editTask,
+                  deletetask: deleteTask,
+                  checkTask: checkTask,
+                  index: i,
+                ),
+          ],
+        ),
+      ]),
+      // body: ListView.builder(
+      //     itemCount: habitListLenght,
+      //     itemBuilder: (context, index) => HabitTile(
+      //         edittask: editTask,
+      //         deletetask: deleteTask,
+      //         checkTask: checkTask,
+      //         index: index)),
       floatingActionButton: FloatingActionButton(
         backgroundColor: const Color.fromARGB(255, 37, 67, 54),
         onPressed: () => showModalBottomSheet(
