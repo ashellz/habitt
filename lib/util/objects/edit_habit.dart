8f8a46bb1e1f385946034b840576d23bd8cdd68f
ashellz <101022734+ashellz@users.noreply.github.com>
2024-06-30 20:31:16 +0200

    Update
    - Implementing edit habit duration and amount

--- lib/util/objects/edit_habit.dart
+++ lib/util/objects/edit_habit.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:habit_tracker/data/habit_tile.dart';
 import 'package:habit_tracker/pages/home_page.dart';
 import 'package:habit_tracker/pages/icons_page.dart';
 import 'package:habit_tracker/util/functions/getIcon.dart';
 import 'package:hive/hive.dart';
+import 'package:numberpicker/numberpicker.dart';
 
 final habitBox = Hive.box<HabitData>('habits');
 
-Widget editHabit(formKey, validateText, deletetask, edithabit, index) {
+Widget editHabit(_formKey, _validateText, deletetask, edithabit, index) {
+  late int habitGoal;
+  late int amountValue;
+  late int durationValue;
+  TextEditingController amountNameController = TextEditingController();
   return StatefulBuilder(
     builder: (BuildContext context, StateSetter mystate) {
       if (!changed) {
@@ -16,11 +22,21 @@ Widget editHabit(formKey, validateText, deletetask, edithabit, index) {
       if (editcontroller.text.isEmpty) {
         editcontroller.text = habitBox.getAt(index)!.name;
       }
+
+      if (habitBox.getAt(index)!.amount > 1) {
+        habitGoal = 1;
+        amountValue = habitBox.getAt(index)!.amountCompleted;
+        amountNameController.text = habitBox.getAt(index)!.amountName;
+      } else {
+        habitGoal = 2;
+        durationValue = habitBox.getAt(index)!.durationCompleted;
+      }
+
       return SingleChildScrollView(
         child: SizedBox(
           height: MediaQuery.of(context).size.height * 0.65,
           child: Form(
-            key: formKey,
+            key: _formKey,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -55,7 +71,7 @@ Widget editHabit(formKey, validateText, deletetask, edithabit, index) {
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 20.0),
                   child: TextFormField(
-                    validator: validateText,
+                    validator: _validateText,
                     controller: editcontroller,
                     decoration: InputDecoration(
                       suffixIcon: IconButton(
@@ -112,6 +128,133 @@ Widget editHabit(formKey, validateText, deletetask, edithabit, index) {
                     ),
                   ),
                 ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          mystate(() {
+                            if (habitGoal == 1) {
+                              habitGoal = 0;
+                            } else {
+                              amountValue = 2;
+                              habitGoal = 1;
+                            }
+                          });
+                        },
+                        style: ButtonStyle(
+                          fixedSize: WidgetStateProperty.all<Size>(
+                              const Size(170, 45)),
+                          backgroundColor: WidgetStateProperty.all<Color>(
+                            habitGoal == 1
+                                ? const Color.fromARGB(255, 107, 138, 122)
+                                : const Color.fromARGB(255, 183, 181, 151),
+                          ),
+                        ),
+                        child: const Text("Number of times",
+                            style: TextStyle(color: Colors.black)),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          mystate(() {
+                            if (habitGoal == 2) {
+                              habitGoal = 0;
+                            } else {
+                              durationValue = 1;
+                              habitGoal = 2;
+                            }
+                          });
+                        },
+                        style: ButtonStyle(
+                          fixedSize: WidgetStateProperty.all<Size>(
+                              const Size(170, 45)),
+                          backgroundColor: WidgetStateProperty.all<Color>(
+                            habitGoal == 2
+                                ? const Color.fromARGB(255, 107, 138, 122)
+                                : const Color.fromARGB(255, 183, 181, 151),
+                          ),
+                        ),
+                        child: const Text("Duration",
+                            style: TextStyle(color: Colors.black)),
+                      ),
+                    ],
+                  ),
+                ),
+                Visibility(
+                  visible: habitGoal == 1,
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 20, right: 20, top: 10),
+                    child: Column(
+                      children: [
+                        Center(
+                          child: NumberPicker(
+                            value: amountValue,
+                            minValue: 2,
+                            maxValue: 100,
+                            haptics: true,
+                            axis: Axis.horizontal,
+                            onChanged: (value) =>
+                                mystate(() => amountValue = value),
+                          ),
+                        ),
+                        Center(
+                          child:
+                              Text("$amountValue ${amountNameController.text}"),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        TextFormField(
+                          inputFormatters: [
+                            LengthLimitingTextInputFormatter(10),
+                            LowerCaseTextInputFormatter(),
+                          ],
+                          controller: amountNameController,
+                          decoration: const InputDecoration(
+                              contentPadding:
+                                  EdgeInsets.symmetric(horizontal: 20.0),
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15.0)),
+                              ),
+                              filled: true,
+                              fillColor: Color.fromARGB(255, 183, 181, 151),
+                              label: Text("Amount Name")),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Visibility(
+                  visible: habitGoal == 2,
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 20, right: 20, top: 10),
+                    child: Column(
+                      children: [
+                        Center(
+                          child: NumberPicker(
+                            value: durationValue,
+                            minValue: 1,
+                            maxValue: 100,
+                            haptics: true,
+                            axis: Axis.horizontal,
+                            onChanged: (value) =>
+                                mystate(() => durationValue = value),
+                          ),
+                        ),
+                        Center(
+                          child: Text(durationValue == 1
+                              ? "1 minute"
+                              : "$durationValue minutes"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
                 const Spacer(),
                 Padding(
                   padding:
@@ -141,7 +284,7 @@ Widget editHabit(formKey, validateText, deletetask, edithabit, index) {
                       ),
                       ElevatedButton(
                         onPressed: () {
-                          if (formKey.currentState!.validate()) {
+                          if (_formKey.currentState!.validate()) {
                             edithabit(index);
                           }
                         },
@@ -167,4 +310,15 @@ Widget editHabit(formKey, validateText, deletetask, edithabit, index) {
       );
     },
   );
+}
+
+class LowerCaseTextInputFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    return newValue.copyWith(
+      text: newValue.text.toLowerCase(),
+      selection: newValue.selection,
+    );
+  }
 }
