a4751f3abc65eb007ae03a6bcde1e40ada6c2a63
ashellz <101022734+ashellz@users.noreply.github.com>
2024-07-11 07:06:14 +0200

    Update
    Updating the design

--- lib/pages/add_habit_page.dart
+++ lib/pages/add_habit_page.dart
@@ -1,3 +1,4 @@
+import "package:awesome_notifications/awesome_notifications.dart";
 import "package:flutter/material.dart";
 import "package:flutter/services.dart";
 import "package:habit_tracker/pages/home_page.dart";
@@ -25,259 +26,211 @@ class AddHabitPage extends StatefulWidget {
 class _AddHabitPageState extends State<AddHabitPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.black,
-        body: Form(
-          key: formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(
-                  top: 20.0,
-                  left: 25.0,
-                  bottom: 10.0,
-                ),
-                child: Text(
-                  "New Habit",
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
+    Color theLightGreen = const Color.fromARGB(255, 62, 80, 71);
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: Colors.black,
+          body: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(
+                    top: 20.0,
+                    left: 25.0,
+                    bottom: 10.0,
+                  ),
+                  child: Text(
+                    "New Habit",
+                    style: TextStyle(
+                      fontSize: 32.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: TextFormField(
-                  inputFormatters: [
-                    LengthLimitingTextInputFormatter(35),
-                  ],
-                  validator: validateText,
-                  controller: createcontroller,
-                  decoration: InputDecoration(
-                    suffixIcon: IconButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const IconsPage(),
-                          ),
-                        ).then((value) => setState(() {
-                              updatedIcon = theIcon;
-                            }));
-                      },
-                      icon: updatedIcon,
-                    ),
-                    labelStyle: const TextStyle(fontSize: 16.0),
-                    labelText: "Habit Name",
-                    border: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                    filled: true,
-                    fillColor: const Color.fromARGB(255, 183, 181, 151),
+
+                // ICON
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 10.0,
+                    bottom: 10.0,
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: ButtonTheme(
-                  alignedDropdown: true,
-                  child: DropdownButtonFormField(
-                    dropdownColor: const Color.fromARGB(255, 218, 211, 190),
+                  child: Container(
+                    height: 170,
+                    decoration: BoxDecoration(
+                      color: theLightGreen,
+                    ),
+                    child: Row(
+                      children: [
+                        Wrap(
+                          direction: Axis.horizontal,
+                          spacing: 10,
+                          runSpacing: 10,
+                          alignment: WrapAlignment.center,
+                          crossAxisAlignment: WrapCrossAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10.0),
+                              child: IconButton(
+                                iconSize: 80,
+                                onPressed: () => Navigator.of(context)
+                                    .push(MaterialPageRoute(
+                                        builder: (context) =>
+                                            const IconsPage()))
+                                    .then((value) => setState(() {
+                                          updatedIcon = theIcon;
+                                        })),
+                                icon: updatedIcon,
+                                color: Colors.white,
+                              ),
+                            ),
+
+                            //TEXT
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  truncatedText(context),
+                                  style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  dropDownValue,
+                                  style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                            const Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(left: 45, right: 20),
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                        "15",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 28,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Text(
+                                        "minutes",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold),
+                                      )
+                                    ],
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+
+                //NAME
+                ,
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 20.0, right: 20, top: 20, bottom: 15),
+                  child: TextFormField(
+                    onChanged: (newValue) => setState(() {
+                      createcontroller.text = newValue;
+                    }),
+                    inputFormatters: [
+                      LengthLimitingTextInputFormatter(35),
+                    ],
+                    validator: validateText,
+                    controller: createcontroller,
+                    cursorColor: Colors.white,
+                    cursorWidth: 2.0,
+                    style: const TextStyle(color: Colors.white),
                     decoration: InputDecoration(
-                      contentPadding:
-                          const EdgeInsets.symmetric(horizontal: 20.0),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15.0),
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
                       ),
                       filled: true,
-                      fillColor: const Color.fromARGB(255, 218, 211, 190),
-                    ),
-                    style: const TextStyle(
-                      fontFamily: 'Poppins',
-                      color: Colors.black,
-                      fontSize: 16.0,
-                    ),
-                    hint: const Text("Any Time"),
-                    items: dropdownItems,
-                    onChanged: (String? newValue) {
-                      dropDownValue = newValue!;
-                    },
-                  ),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(left: 20.0),
-                child: Text(
-                  "Habit goal",
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          if (habitGoal == 1) {
-                            habitGoal = 0;
-                          } else {
-                            currentAmountValue = 2;
-                            habitGoal = 1;
-                          }
-                        });
-                      },
-                      style: ButtonStyle(
-                        fixedSize: WidgetStateProperty.all<Size>(
-                            Size(MediaQuery.of(context).size.width * 0.42, 45)),
-                        backgroundColor: WidgetStateProperty.all<Color>(
-                          habitGoal == 1
-                              ? const Color.fromARGB(255, 107, 138, 122)
-                              : const Color.fromARGB(255, 183, 181, 151),
-                        ),
-                      ),
-                      child: const Text("Number of times",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(color: Colors.black)),
-                    ),
-                    const SizedBox(
-                      width: 15,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          if (habitGoal == 2) {
-                            habitGoal = 0;
-                          } else {
-                            currentDurationValue = 1;
-                            habitGoal = 2;
-                          }
-                        });
-                      },
-                      style: ButtonStyle(
-                        fixedSize: WidgetStateProperty.all<Size>(
-                            Size(MediaQuery.of(context).size.width * 0.43, 45)),
-                        backgroundColor: WidgetStateProperty.all<Color>(
-                          habitGoal == 2
-                              ? const Color.fromARGB(255, 107, 138, 122)
-                              : const Color.fromARGB(255, 183, 181, 151),
-                        ),
-                      ),
-                      child: const Text("Duration",
-                          style: TextStyle(color: Colors.black)),
-                    ),
-                  ],
-                ),
-              ),
-              Visibility(
-                visible: habitGoal == 1,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
-                  child: Column(
-                    children: [
-                      Center(
-                        child: NumberPicker(
-                          value: currentAmountValue,
-                          minValue: 2,
-                          maxValue: 100,
-                          haptics: true,
-                          axis: Axis.horizontal,
-                          onChanged: (value) =>
-                              setState(() => currentAmountValue = value),
-                        ),
-                      ),
-                      Center(
-                        child: Text(
-                            "$currentAmountValue ${amountNameController.text}"),
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      TextFormField(
-                        inputFormatters: [
-                          LengthLimitingTextInputFormatter(10),
-                          LowerCaseTextInputFormatter(),
-                        ],
-                        validator: validateText,
-                        controller: amountNameController,
-                        decoration: const InputDecoration(
-                            contentPadding:
-                                EdgeInsets.symmetric(horizontal: 20.0),
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(15.0)),
-                            ),
-                            filled: true,
-                            fillColor: Color.fromARGB(255, 183, 181, 151),
-                            label: Text("Amount Name")),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Visibility(
-                visible: habitGoal == 2,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
-                  child: Column(
-                    children: [
-                      Center(
-                        child: NumberPicker(
-                          value: currentDurationValue,
-                          minValue: 1,
-                          maxValue: 100,
-                          haptics: true,
-                          axis: Axis.horizontal,
-                          onChanged: (value) =>
-                              setState(() => currentDurationValue = value),
-                        ),
-                      ),
-                      Center(
-                        child: Text(currentDurationValue == 1
-                            ? "1 minute"
-                            : "$currentDurationValue minutes"),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const Spacer(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      right: 20.0,
-                      bottom: 20.0,
-                    ),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (formKey.currentState!.validate()) {
-                          widget.createNewHabit();
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 37, 67, 54),
-                        shape: const StadiumBorder(),
-                        minimumSize: const Size(120, 50),
-                      ),
-                      child: const Text(
-                        "Add",
+                      fillColor: theLightGreen,
+                      label: const Text(
+                        "Habit Name",
                         style: TextStyle(color: Colors.white),
                       ),
                     ),
                   ),
-                ],
-              ),
-            ],
-          ),
-        ));
+                ),
+
+                // DROPDOWN MENU
+                Padding(
+                  padding:
+                      const EdgeInsets.only(left: 20.0, right: 20, bottom: 15),
+                  child: ButtonTheme(
+                    alignedDropdown: true,
+                    child: DropdownButtonFormField(
+                      dropdownColor: theLightGreen,
+                      decoration: InputDecoration(
+                        contentPadding:
+                            const EdgeInsets.symmetric(horizontal: 20.0),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        filled: true,
+                        fillColor: theLightGreen,
+                      ),
+                      style: const TextStyle(
+                        fontFamily: 'Poppins',
+                        color: Colors.white,
+                        fontSize: 16.0,
+                      ),
+                      hint: const Text("Any Time",
+                          style: TextStyle(color: Colors.white)),
+                      items: dropdownItems,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropDownValue = newValue!;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )),
+    );
   }
+}
+
+String truncatedText(BuildContext context) {
+  double screenWidth = MediaQuery.of(context).size.width;
+  int maxLength;
+
+  if (screenWidth < 300) {
+    maxLength = 5; // very small screen
+  } else if (screenWidth < 400) {
+    maxLength = 8; // small screen
+  } else if (screenWidth < 500) {
+    maxLength = 12; // medium screen
+  } else {
+    maxLength = 20; // larger screen
+  }
+
+  String name = createcontroller.text;
+  if (name.length > maxLength) {
+    return '${name.substring(0, maxLength)}...';
+  }
+  return name;
 }
